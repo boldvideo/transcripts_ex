004478d96e9b2ba7d0f43243408801a6e17c0251
Marcel Fahle <m.fahle@gmail.com>
2024-03-18 05:54:49 +0100

    0.2.1 adds chapter conversion to public api

--- lib/convert.ex
+++ lib/convert.ex
@@ -3,11 +3,19 @@ defmodule BoldTranscriptsEx.Convert do
 
   def from(service, transcript_data, opts \\ [])
 
-  def from(:assemblyai, transcript_data, opts) do
-    BoldTranscriptsEx.Convert.AssemblyAI.convert(transcript_data, opts)
+  def from(:assemblyai, transcript_json, opts) do
+    BoldTranscriptsEx.Convert.AssemblyAI.transcript_to_bold(transcript_json, opts)
   end
 
   def from(service, _transcript_data, _opts) do
-    IO.puts("Conversion from #{service} is not supported yet.")
+    Logger.error("Conversion from #{service} is not implemented yet.")
+  end
+
+  def chapters_to_webvtt(:assemblyai, transcript_json, opts) do
+    BoldTranscriptsEx.Convert.AssemblyAI.chapters_to_webvtt(transcript_json, opts)
+  end
+
+  def chapters_to_webvtt(service, _transcript_json, _opts) do
+    Logger.error("Conversion from #{service} is not implemented yet.")
   end
 end
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BoldTranscriptsEx.MixProject do
   def project do
     [
       app: :bold_transcripts_ex,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
