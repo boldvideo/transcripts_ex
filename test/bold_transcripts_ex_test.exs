4f0803f295137f39da8aa3197160e8ada5001dc5
Peter Ullrich <PJUllrich@users.noreply.github.com>
2024-04-09 11:06:11 +0200

    Add webvtt to text.

--- test/bold_transcripts_ex_test.exs
+++ test/bold_transcripts_ex_test.exs
@@ -1,29 +1,11 @@
 defmodule BoldTranscriptsExTest do
   use ExUnit.Case
-  doctest BoldTranscriptsEx
 
-  alias BoldTranscriptsEx.WebVTT
+  doctest BoldTranscriptsEx
 
   require Logger
 
-  defp load_json_from_file(path) do
-    path
-    |> File.read!()
-
-    # |> Jason.decode!()
-  end
-
-  # describe "Bold Transcripts to WebVTT" do
-  #   test "greets the world" do
-  #     input = load_json_from_file("test/support/data/bold_transcript_howard.json") |> IO.inspect()
-  #
-  #     vtt = BoldTranscriptsEx.WebVTT2.create(input)
-  #
-  #     Logger.info(inspect(vtt))
-  #
-  #     assert match?({:ok, _}, vtt)
-  #   end
-  # end
+  defp load_json_from_file(path), do: File.read!(path)
 
   describe "AssemblyAI Transcripts" do
     test "chapters to WebVTT" do
